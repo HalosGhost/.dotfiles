88eaf35f4ff00a32e5457bce8eb1f7e046e6c8a7
HalosGhost <samuel.stuewe@gmail.com>
2013-05-01 11:15:54 +0200

    Use custom theme

--- alopex/theme.h
+++ alopex/theme.h
@@ -1,7 +1,7 @@
 /* copyright J.McClure 2012-2013, see alopex.c or COPYING for details */
 
 static const char colors[LASTColor][9] = {
-    #ifdef SummerCoat
+/*    #ifdef SummerCoat
 	    [Background]	= "#201814",
 	    [Default]		= "#686858",
 	    [Occupied]		= "#DDBB88",
@@ -29,20 +29,20 @@ static const char colors[LASTColor][9] = {
 	    [TabDefault]	= "#686868",
 	    [TabDefaultBG]	= "#181818",
 	    [TagLine]		= "#2436AA",
-    #else
-        [Background]      = "#101010",
-        [Default]         = "#6C6C6C",
-        [Occupied]        = "#C6C6C6",
-        [Selected]        = "#4C7899",
-        [Urgent]          = "#FF8880",
-        [Title]           = "#DDDDDD",
-        [TabFocused]      = "#4C7899",
-        [TabFocusedBG]    = "#333333",
-        [TabTop]          = "#6C6C6C",
-        [TabTopBG]        = "#333333",
-        [TabDefault]      = "#101010",
-        [TabDefaultBG]    = "#333333",
-        [TagLine]         = "#4C7899",
-    #endif
+    #else */
+        [Background]    = "#101010",
+        [Default]       = "#6C6C6C",
+        [Occupied]      = "#C6C6C6",
+        [Selected]      = "#4C7899",
+        [Urgent]        = "#FF8880",
+        [Title]         = "#DDDDDD",
+        [TabFocused]    = "#4C7899",
+        [TabFocusedBG]  = "#333333",
+        [TabTop]        = "#6C6C6C",
+        [TabTopBG]      = "#333333",
+        [TabDefault]    = "#101010",
+        [TabDefaultBG]  = "#333333",
+        //[TagLine]       = "#4C7899",
+    //#endif
 };
 
