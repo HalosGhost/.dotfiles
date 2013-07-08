f929a1a38649aa06e23b85e1d6e7cd209a03df00
HalosGhost <halosghost@archlinux.info>
2013-07-08 09:45:01 -0500

    Fix and rename Modifiers; Update Function keybinds

--- alopex/config.h
+++ alopex/config.h
@@ -30,21 +30,22 @@ static int           stackcount           = 2;
 #define LAUNCH       "interrobang"
 #define TERM         "termite"
 #define LOCK         "xautolock -locknow"
+#define WNDWLST      "interrobang -opt \"set last = false\" alopex"
 #define SCRN         "scrot $HOME/Pictures/Scrn/shot-%d-%m-%Y.png"
 #define CMD(app)     app "&"
 #define XRANDR_CMD   "xrandr --output LVDS --auto --output DisplayPort-0 --auto --left-of LVDS"
 #define WALLPAPER    "source $HOME/.fehbg"
 
 /* key definitions: */
-#define MOD1 Mod4Mask
-#define MOD2 Mod1Mask
-#define MOD3 ControlMask
-#define MOD4 ShiftMask
+#define ALTR Mod1Mask
+#define CTRL ControlMask
+#define SHFT ShiftMask
+#define SUPR Mod4Mask
 #define TAGK(KEY,TAG) \
-   { MOD1,           KEY,           tag,           "t " TAG          }, \
-   { MOD1|MOD2,      KEY,           tag,           "m " TAG          }, \
-   { MOD1|MOD3,      KEY,           tag,           "s " TAG          }, \
-   { MOD1|MOD4,      KEY,           tag,           "a " TAG          },
+   { ALTR,           KEY,           tag,           "t " TAG          }, \
+   { ALTR|SUPR,      KEY,           tag,           "m " TAG          }, \
+   { ALTR|CTRL,      KEY,           tag,           "s " TAG          }, \
+   { ALTR|SHFT,      KEY,           tag,           "a " TAG          },
 
 #define FN1          "sudo enlighten decrease display"
 #define FN2          "sudo enlighten increase display"
@@ -56,43 +57,41 @@ static int           stackcount           = 2;
 #define FNB          "ponymix decrease 5"
 #define FNC          "ponymix increase 5"
 
-
 static Key keys[] = {
    /* modifier       key            function       argument          */
    /* launchers + misc: */
-   { MOD1,           XK_Return,     spawn,         CMD(TERM)         },
-   { MOD1,           XK_d,          spawn,         CMD(LAUNCH)       },
-   { MOD1,           XK_w,          spawn,         CMD("$BROWSER")   },
-   { MOD2|MOD4,      XK_q,          quit,          NULL              },
-   { MOD1|MOD4,      XK_q,          killclient,    NULL              },
-   { MOD1,           XK_f,          fullscreen,    NULL              },
-   { MOD1,           XK_a,          toggle,        "place bar"       },
-   { MOD1,           XK_x,          toggle,        "visible bar"     },
-   { MOD1|MOD2,      XK_f,          toggle,        "floating"        },
-   { MOD2|MOD3,      XK_l,          spawn,         CMD(LOCK)         },
-   { MOD1,           0x60,          spawn,         CMD(SCRN)         },
-   { MOD1,           XK_F4,         spawn,         XRANDR_CMD,       },
+   { ALTR,           XK_Return,     spawn,         CMD(TERM)         },
+   { ALTR,           XK_d,          spawn,         CMD(LAUNCH)       },
+   { ALTR,           XK_w,          spawn,         CMD("$BROWSER")   },
+   { SUPR|SHFT,      XK_q,          quit,          NULL              },
+   { ALTR|SHFT,      XK_q,          killclient,    NULL              },
+   { ALTR,           XK_f,          fullscreen,    NULL              },
+   { ALTR,           XK_a,          toggle,        "place bar"       },
+   { ALTR,           XK_x,          toggle,        "visible bar"     },
+   { ALTR|SUPR,      XK_f,          toggle,        "floating"        },
+   { SUPR|CTRL,      XK_l,          spawn,         CMD(LOCK)         },
+   { ALTR,           0x60,          spawn,         CMD(SCRN)         },
+   { ALTR,           XK_F4,         spawn,         XRANDR_CMD        },
    /* function keys */
    { 0,              XK_F1,         spawn,         CMD(FN1)          },
    { 0,              XK_F2,         spawn,         CMD(FN2)          },
-   { 0,              XK_F3,         spawn,         CMD(FN3)          },
-   { 0,              XK_F4,         spawn,         CMD(FN4)          },
-   { 0,              XK_F5,         spawn,         CMD(FN5)          },
-   { 0,              XK_F6,         spawn,         CMD(FN6)          },
-   { 0,              XK_F10,        spawn,         CMD(FNA)          },
-   { 0,              XK_F11,        spawn,         CMD(FNB)          },
-   { 0,              XK_F12,        spawn,         CMD(FNC)          },
+   { 0,              XK_F9,         spawn,         CMD(FNA)          },
+   { 0,              XK_F11,        spawn,         CMD(FN3)          },
+   { 0,              XK_F12,        spawn,         CMD(FN4)          },
+   { 0,              0x1008ff11,    spawn,         CMD(FNB)          },
+   { 0,              0x1008ff13,    spawn,         CMD(FNC)          },
    { 0,              0x1008ff2c,    spawn,         CMD("eject")      },
+   { 0,              XK_Menu,       windowlist,    WNDWLST           },
    /* tiling: */
-   { MOD1,           XK_space,      tile,          "cycle"           },
-   { MOD1,           XK_i,          tile_conf,     "increase"        },
-   { MOD1,           XK_o,          tile_conf,     "decrease"        },
-   { MOD1,           XK_equal,      tile_conf,     "+"               },
-   { MOD1,           XK_minus,      tile_conf,     "-"               },
-   { MOD1,           XK_period,     tile_conf,     "all"             },
-   { MOD1,           XK_comma,      tile_conf,     "one"             },
+   { ALTR,           XK_space,      tile,          "cycle"           },
+   { ALTR,           XK_i,          tile_conf,     "increase"        },
+   { ALTR,           XK_o,          tile_conf,     "decrease"        },
+   { ALTR,           XK_equal,      tile_conf,     "+"               },
+   { ALTR,           XK_minus,      tile_conf,     "-"               },
+   { ALTR,           XK_period,     tile_conf,     "all"             },
+   { ALTR,           XK_comma,      tile_conf,     "one"             },
    /* tags and views: */
-   { MOD1,           XK_Tab,        tag,           "flip"            },
+   { ALTR,           XK_Tab,        tag,           "flip"            },
      TAGK(           XK_1,                         "1"               )
      TAGK(           XK_2,                         "2"               )
      TAGK(           XK_3,                         "3"               )
@@ -102,30 +101,30 @@ static Key keys[] = {
      TAGK(           XK_7,                         "7"               )
    /* window focus/movement:
       f=focus, s=swap                                                */
-   { MOD1,           XK_k,          window,        "f prev"          },
-   { MOD1,           XK_Left,       window,        "f prev"          },
-   { MOD1,           XK_j,          window,        "f next"          },
-   { MOD1,           XK_Right,      window,        "f next"          },
-   { MOD2,           XK_Tab,        window,        "f alt"           },
-   { MOD1,           XK_h,          window,        "s prev"          },
-   { MOD1,           XK_Up,         window,        "s prev"          },
-   { MOD1,           XK_l,          window,        "s next"          },
-   { MOD1,           XK_Down,       window,        "s next"          },
-   { MOD2|MOD4,      XK_Tab,        window,        "s alt"           },
-   { MOD1|MOD4,      XK_equal,      window,        "+"               },
-   { MOD1|MOD4,      XK_minus,      window,        "-"               },
-   { MOD1|MOD4,      XK_space,      toggle,        "monitor focus"   },
+   { ALTR,           XK_k,          window,        "f prev"          },
+   { ALTR,           XK_Left,       window,        "f prev"          },
+   { ALTR,           XK_j,          window,        "f next"          },
+   { ALTR,           XK_Right,      window,        "f next"          },
+   { SUPR,           XK_Tab,        window,        "f alt"           },
+   { ALTR,           XK_h,          window,        "s prev"          },
+   { ALTR,           XK_Up,         window,        "s prev"          },
+   { ALTR,           XK_l,          window,        "s next"          },
+   { ALTR,           XK_Down,       window,        "s next"          },
+   { SUPR|SHFT,      XK_Tab,        window,        "s alt"           },
+   { ALTR|SHFT,      XK_equal,      window,        "+"               },
+   { ALTR|SHFT,      XK_minus,      window,        "-"               },
+   { ALTR|SHFT,      XK_space,      toggle,        "monitor focus"   },
 };
 
 static Button buttons[] = {
    /* modifier       button         function       arg               */
-   { MOD1|MOD2,      1,             mouse,         "move"            },
-   { MOD1,           2,             toggle,        "floating"        },
-   { MOD1,           3,             mouse,         "resize"          },
-   { MOD1,           4,             window,        "s prev"          },
-   { MOD1,           5,             window,        "s next"          },
-   { MOD1,           6,             window,        "f prev"          },
-   { MOD1,           7,             window,        "f next"          },
+   { ALTR|SUPR,      1,             mouse,         "move"            },
+   { ALTR,           2,             toggle,        "floating"        },
+   { ALTR,           3,             mouse,         "resize"          },
+   { ALTR,           4,             window,        "s prev"          },
+   { ALTR,           5,             window,        "s next"          },
+   { ALTR,           6,             window,        "f prev"          },
+   { ALTR,           7,             window,        "f next"          },
 };
 
 static Rule rules[] = {
