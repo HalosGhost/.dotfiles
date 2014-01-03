d476d5879190bec4ce866de981645569976ec2cc
Sam Stuewe <halosghost@archlinux.info>
2014-01-02 18:33:29 -0600

    Retab source

--- bin/lsip.c
+++ bin/lsip.c
@@ -4,26 +4,27 @@
 
 int main (int argc, char ** argv)
 {   CURL * handle;
-	CURLcode res;
+    CURLcode res;
 
-	curl_global_init(CURL_GLOBAL_ALL);
-	handle = curl_easy_init();
+    curl_global_init(CURL_GLOBAL_ALL);
+    handle = curl_easy_init();
 
-	if ( handle )
+    if ( handle )
     {   curl_easy_setopt(handle, CURLOPT_URL, "http://icanhazip.com");
-		curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1);
+        curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1);
 
-		res = curl_easy_perform(handle);
-		if ( ! res == CURLE_OK )
+        res = curl_easy_perform(handle);
+        if ( ! res == CURLE_OK )
         {   curl_easy_cleanup(handle);
-			curl_global_cleanup();
-			fputs("Could not check IP address", stderr);
-			exit(1);
-		}
-	}
-	
-	curl_easy_cleanup(handle);
-	curl_global_cleanup();
+            curl_global_cleanup();
+            fputs("Could not check IP address", stderr);
+            exit(1);
+        }
+    }
+    
+    curl_easy_cleanup(handle);
+    curl_global_cleanup();
 
-	return 0;
+    return 0;
 }
+// vim: set tabstop=4 shiftwidth=4 expandtab
