0a27994323ea1bfe5b5ced4deb95e3628fc03513
halosghost <halosghost@archlinux.info>
2016-04-12 20:58:44 -0500

    Use single-statement arrows to avoid braces

--- keysnail.js
+++ keysnail.js
@@ -37,34 +37,39 @@ var closeNonPinnedTab = function () {
     }
 }
 
+var yankCurrentURL    = function () {
+    let url = bmui._uri.asciiSpec;
+    scpt(url); secho('Yanked ' + url);
+}
+
 var viewKs =
-[ [ 'h',       ()     => { goDoCommand('cmd_scrollLeft');                                    }, 'Scroll Left'     ]
-, [ 'j',       ()     => { goDoCommand('cmd_scrollLineDown');                                }, 'Scroll Down'     ]
-, [ 'k',       ()     => { goDoCommand('cmd_scrollLineUp');                                  }, 'Scroll Up'       ]
-, [ 'l',       ()     => { goDoCommand('cmd_scrollRight');                                   }, 'Scroll Right'    ]
-, [ ['g','g'], ()     => { goDoCommand('cmd_scrollTop');                                     }, 'Scroll Top'      ]
-, [ 'G',       ()     => { goDoCommand('cmd_scrollBottom');                                  }, 'Scroll Bottom'   ]
-, [ 'H',       ()     => { BrowserBack();                                                    }, 'Hist Back'       ]
-, [ 'J',       ()     => { getBrowser().mTabContainer.advanceSelectedTab( 1, true);          }, 'Next Tab'        ]
-, [ 'K',       ()     => { getBrowser().mTabContainer.advanceSelectedTab(-1, true);          }, 'Prev Tab'        ]
-, [ 'L',       ()     => { BrowserForward();                                                 }, 'Hist Forward'    ]
-, [ ':',       (e, a) => { shell.input(null, a);                                             }, 'Command Mode'    ]
-, [ 'r',       ()     => { BrowserReload();                                                  }, 'Reload'          ]
-, [ 'R',       ()     => { BrowserReloadSkipCache();                                         }, 'Reload (cache)'  ]
-, [ 'd',       ()     => { closeNonPinnedTab();                                              }, 'Close Tab'       ]
-, [ 'u',       ()     => { undoCloseTab();                                                   }, 'Unclose Tab'     ]
-, [ 't',       ()     => { shell.input('tabopen ');                                          }, 'Tabopen'         ]
-, [ 'T',       ()     => { shell.input('tabopen ' + bmui._uri.asciiSpec);                    }, 'Tabopen URL'     ]
-, [ 'o',       ()     => { shell.input('open ');                                             }, 'Open'            ]
-, [ 'O',       ()     => { shell.input('open ' + bmui._uri.asciiSpec);                       }, 'Open URL'        ]
-, [ 'y',       ()     => { let url = bmui._uri.asciiSpec; scpt(url); secho('Yanked ' + url); }, 'Yank URL'        ]
-, [ 'p',       ()     => { gBrowser.loadURI(gcpt(), null, null);                             }, 'Open Yanked'     ]
-, [ 'P',       ()     => { gBrowser.loadOneTab(gcpt(), null, null, null, false);             }, 'Tabopen Yanked'  ]
-, [ '/',       ()     => { command.iSearchForward();                                         }, 'Search Forward'  ]
-, [ '?',       ()     => { command.iSearchBackward();                                        }, 'Search Backward' ]
-, [ '[',       ()     => { gBrowser.loadOneTab('about:about', null, null, null, false);      }, 'about:about'     ]
-, [ ['Z','Q'], ()     => { goQuitApplication();                                              }, 'Close Firefox'   ]
-, [ ['Z','R'], ()     => { command.restartApp();                                             }, 'Restart Firefox' ]
+[ [ 'h',       ()     => goDoCommand('cmd_scrollLeft')                              , 'Scroll Left'     ]
+, [ 'j',       ()     => goDoCommand('cmd_scrollLineDown')                          , 'Scroll Down'     ]
+, [ 'k',       ()     => goDoCommand('cmd_scrollLineUp')                            , 'Scroll Up'       ]
+, [ 'l',       ()     => goDoCommand('cmd_scrollRight')                             , 'Scroll Right'    ]
+, [ ['g','g'], ()     => goDoCommand('cmd_scrollTop')                               , 'Scroll Top'      ]
+, [ 'G',       ()     => goDoCommand('cmd_scrollBottom')                            , 'Scroll Bottom'   ]
+, [ 'H',       ()     => BrowserBack()                                              , 'Hist Back'       ]
+, [ 'J',       ()     => getBrowser().mTabContainer.advanceSelectedTab( 1, true)    , 'Next Tab'        ]
+, [ 'K',       ()     => getBrowser().mTabContainer.advanceSelectedTab(-1, true)    , 'Prev Tab'        ]
+, [ 'L',       ()     => BrowserForward()                                           , 'Hist Forward'    ]
+, [ ':',       (e, a) => shell.input(null, a)                                       , 'Command Mode'    ]
+, [ 'r',       ()     => BrowserReload()                                            , 'Reload'          ]
+, [ 'R',       ()     => BrowserReloadSkipCache()                                   , 'Reload (cache)'  ]
+, [ 'd',       ()     => closeNonPinnedTab()                                        , 'Close Tab'       ]
+, [ 'u',       ()     => undoCloseTab()                                             , 'Unclose Tab'     ]
+, [ 't',       ()     => shell.input('tabopen ')                                    , 'Tabopen'         ]
+, [ 'T',       ()     => shell.input('tabopen ' + bmui._uri.asciiSpec)              , 'Tabopen URL'     ]
+, [ 'o',       ()     => shell.input('open ')                                       , 'Open'            ]
+, [ 'O',       ()     => shell.input('open ' + bmui._uri.asciiSpec)                 , 'Open URL'        ]
+, [ 'y',       ()     => yankCurrentURL()                                           , 'Yank URL'        ]
+, [ 'p',       ()     => gBrowser.loadURI(gcpt(), null, null)                       , 'Open Yanked'     ]
+, [ 'P',       ()     => gBrowser.loadOneTab(gcpt(), null, null, null, false)       , 'Tabopen Yanked'  ]
+, [ '/',       ()     => command.iSearchForward()                                   , 'Search Forward'  ]
+, [ '?',       ()     => command.iSearchBackward()                                  , 'Search Backward' ]
+, [ '[',       ()     => gBrowser.loadOneTab('about:about', null, null, null, false), 'about:about'     ]
+, [ ['Z','Q'], ()     => goQuitApplication()                                        , 'Close Firefox'   ]
+, [ ['Z','R'], ()     => command.restartApp()                                       , 'Restart Firefox' ]
 ]
 
 for ( let i = 0; i < viewKs.length; ++ i ) {
@@ -72,9 +77,9 @@ for ( let i = 0; i < viewKs.length; ++ i ) {
 }
 
 var editKs =
-[ [ 'C-u',     ()     => { goDoCommand('cmd_deleteToBeginningOfLine');                       }, 'Backline'        ]
-, [ 'C-w',     ()     => { goDoCommand('cmd_deleteWordBackward');                            }, 'Backword'        ]
-, [ 'ESC',     ()     => { document.activeElement.blur();                                    }, 'Escape Focus'    ]
+[ [ 'C-u',     ()     => goDoCommand('cmd_deleteToBeginningOfLine')                 , 'Backline'        ]
+, [ 'C-w',     ()     => goDoCommand('cmd_deleteWordBackward')                      , 'Backword'        ]
+, [ 'ESC',     ()     => document.activeElement.blur()                              , 'Escape Focus'    ]
 ]
 
 for ( let i = 0; i < editKs.length; ++ i ) {
