dc48967588fa0aa3cbeb45eba872de2c0ba30283
halosghost <halosghost@archlinux.info>
2017-07-10 23:22:43 -0500

    update for fancy new st

--- st_config.h
+++ st_config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Inconsolatazi4 for Powerline:pixelsize=15";
-static int borderpx = 0;
+char font[] = "Inconsolatazi4 for Powerline:pixelsize=15";
+int borderpx = 0;
 static char shell[] = "/bin/sh";
 static char *utmp = NULL;
 static char stty_args[] = "stty raw pass8 nl -echo -iexten -cstopb 38400";
@@ -15,8 +15,8 @@ static char stty_args[] = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 static char vtiden[] = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
-static float cwscale = 1.0;
-static float chscale = 1.0;
+float cwscale = 1.0;
+float chscale = 1.0;
 
 /*
  * word delimiter string
@@ -26,23 +26,22 @@ static float chscale = 1.0;
 static char worddelimiters[] = " ";
 
 /* selection timeouts (in milliseconds) */
-static unsigned int doubleclicktimeout = 300;
-static unsigned int tripleclicktimeout = 600;
+unsigned int doubleclicktimeout = 300;
+unsigned int tripleclicktimeout = 600;
 
 /* alt screens */
-static int allowaltscreen = 1;
+int allowaltscreen = 1;
 
 /* frames per second st should at maximum draw to the screen */
-static unsigned int xfps = 120;
-static unsigned int actionfps = 30;
+unsigned int xfps = 120;
+unsigned int actionfps = 30;
 
 /*
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
  * attribute.
  */
-static unsigned int blinktimeout = 800;
-
-static unsigned int cursorthickness = 2;
+unsigned int blinktimeout = 800;
+unsigned int cursorthickness = 2;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
@@ -51,13 +50,13 @@ static unsigned int cursorthickness = 2;
 static int bellvolume = 0;
 
 /* TERM value */
-static char termname[] = "st-256color";
+char termname[] = "st-256color";
 
 static unsigned int tabspaces = 4;
 
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
+const char *colorname[] = {
     /* 8 normal colors */
     "#3f3f3f",
     "#705050",
@@ -90,10 +89,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-static unsigned int defaultfg  = 7;
-static unsigned int defaultbg  = 257;
-static unsigned int defaultcs  = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg  = 7;
+unsigned int defaultbg  = 257;
+unsigned int defaultcs  = 256;
+unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
@@ -102,28 +101,28 @@ static unsigned int defaultrcs = 257;
  * 6: IBeam
  */
 
-static unsigned int cursorshape = 2;
+unsigned int cursorshape = 2;
 
-static unsigned int cols = 80;
-static unsigned int rows = 24;
+unsigned int cols = 80;
+unsigned int rows = 24;
 
 /*
  * Default colour and shape of the mouse cursor
  */
-static unsigned int mouseshape = XC_xterm;
-static unsigned int mousefg = 7;
-static unsigned int mousebg = 0;
+unsigned int mouseshape = XC_xterm;
+unsigned int mousefg = 7;
+unsigned int mousebg = 0;
 
 /*
  * Colors used, when the specific fg == defaultfg. So in reverse mode this
  * will reverse too. Another logic would only make the simple feature too
  * complex.
  */
-static unsigned int defaultattr = 11;
+unsigned int defaultattr = 11;
 
 /* Internal mouse shortcuts. */
 /* Beware that overloading Button1 will disable the selection. */
-static MouseShortcut mshortcuts[] = {
+MouseShortcut mshortcuts[] = {
     /* button               mask            string */
     { Button4,              XK_ANY_MOD,     "\031" },
     { Button5,              XK_ANY_MOD,     "\005" },
@@ -131,22 +130,23 @@ static MouseShortcut mshortcuts[] = {
 
 /* Internal keyboard shortcuts. */
 #define MODKEY Mod1Mask
+#define TERMMOD (MODKEY|ShiftMask)
 
-static Shortcut shortcuts[] = {
-    /* mask                 keysym          function        argument */
-    { XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
-    { ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-    { ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-    { XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-    { MODKEY|ShiftMask,     XK_Prior,       xzoom,          {.f = +1} },
-    { MODKEY|ShiftMask,     XK_Next,        xzoom,          {.f = -1} },
-    { MODKEY|ShiftMask,     XK_Home,        xzoomreset,     {.f =  0} },
-    { ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-    { MODKEY|ShiftMask,     XK_Insert,      clippaste,      {.i =  0} },
-    { MODKEY|ShiftMask,     XK_C,           clipcopy,       {.i =  0} },
-    { MODKEY|ShiftMask,     XK_V,           clippaste,      {.i =  0} },
-    { MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
-    { MODKEY,               XK_Control_L,   iso14755,       {.i =  0} },
+Shortcut shortcuts[] = {
+    /* mask        keysym          function        argument */
+    { XK_ANY_MOD,  XK_Break,       sendbreak,      {.i =  0} },
+    { ControlMask, XK_Print,       toggleprinter,  {.i =  0} },
+    { ShiftMask,   XK_Print,       printscreen,    {.i =  0} },
+    { XK_ANY_MOD,  XK_Print,       printsel,       {.i =  0} },
+    { TERMMOD,     XK_Prior,       zoom,           {.f = +1} },
+    { TERMMOD,     XK_Next,        zoom,           {.f = -1} },
+    { TERMMOD,     XK_Home,        zoomreset,      {.f =  0} },
+    { TERMMOD,     XK_Insert,      selpaste,       {.i =  0} },
+    { TERMMOD,     XK_Insert,      clippaste,      {.i =  0} },
+    { TERMMOD,     XK_C,           clipcopy,       {.i =  0} },
+    { TERMMOD,     XK_V,           clippaste,      {.i =  0} },
+    { MODKEY,      XK_Num_Lock,    numlock,        {.i =  0} },
+    { MODKEY,      XK_Control_L,   iso14755,       {.i =  0} },
 };
 
 /*
@@ -189,7 +189,7 @@ static uint ignoremod = Mod2Mask|XK_SWITCH_MOD;
 /* Override mouse-select while mask is active (when MODE_MOUSE is set).
  * Note that if you want to use ShiftMask with selmasks, set this to an other
  * modifier, set to 0 to not use it. */
-static uint forceselmod = ShiftMask;
+uint forceselmod = ShiftMask;
 
 static Key key[] = {
     /* keysym           mask            string      appkey appcursor crlf */
@@ -397,11 +397,11 @@ static Key key[] = {
  * ButtonRelease and MotionNotify.
  * If no match is found, regular selection is used.
  */
-static uint selmasks[] = {
+uint selmasks[] = {
     [SEL_RECTANGULAR] = ShiftMask,
 };
 
-static char ascii_printable[] =
+char ascii_printable[] =
 	" !\"#$%&'()*+,-./0123456789:;<=>?"
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
